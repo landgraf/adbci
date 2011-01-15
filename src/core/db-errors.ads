375b6d064f6e621747c981f4bb28063ad2005869
John Vinters <john.vinters@googlemail.com>
2011-01-15 22:11:18 +0000

    Licence header update

--- src/core/db-errors.ads
+++ src/core/db-errors.ads
@@ -1,7 +1,7 @@
 --
 --  (c) Copyright 2011, John Vinters
 --
---  This library is free software; you can redistribute it and/or 
+--  ADBC is free software; you can redistribute it and/or 
 --  modify it under the terms of the GNU Lesser General Public License 
 --  as published by the Free Software Foundation; either version 3, or 
 --  (at your option) any later version.  
